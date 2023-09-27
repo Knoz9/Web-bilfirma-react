24d5677e25aa07c13b53c3454bb32a7ca7d926b4
Kenan Maslan <km222ug@student.lnu.se>
2023-09-27 17:54:49 +0200

    test

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,8 @@
 // Will return whether the current environment is in a regular browser
 // and not CEF
 export const isEnvBrowser = (): boolean => !(window as any).invokeNative
+
+// Basic no operation function
+// This function is intentionally empty because...
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export const noop = () => {}
