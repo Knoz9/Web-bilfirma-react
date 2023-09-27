b962859e6cc5c9c1f9717c6d7b683b4a24502bd5
Kenan Maslan <km222ug@student.lnu.se>
2023-09-27 17:43:23 +0200

    Test

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      // Add any other rules you want to modify here
+    },
+  };
+  
