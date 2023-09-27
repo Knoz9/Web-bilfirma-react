acc569dc072982f0eac97018b7b3d9f7b798add3
Kenan Maslan <km222ug@student.lnu.se>
2023-09-27 17:47:28 +0200

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
+    extends: [
+      'plugin:@typescript-eslint/recommended', // TypeScript rules
+      'plugin:react-hooks/recommended', // React hooks rules
+    ],
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
       "react-hooks/exhaustive-deps": "warn",
